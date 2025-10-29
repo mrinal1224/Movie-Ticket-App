14f10bb1fb756f23fcfa2d7eb19fe0f2a16a4814
mrinal1224 <iammrinalkc@gmail.com>
2025-10-29 21:52:30 +0530

    push

---
+++ project/server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dbConfig = require('./dbConfig.js')
+const dotEnv = require('dotenv')
+dotEnv.config()
+
+const app = express()
+dbConfig.connectDb()
+
+app.get('/' , (req , res)=>{
+    res.send('Hello from the Server')
+})
+
+app.listen(8001 , ()=>{
+    console.log('Server Started')
+})
+
+
+
