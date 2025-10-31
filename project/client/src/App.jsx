3e27220525bd14d40e9b7dac0ebfe6a4422309e7
mrinal1224 <iammrinalkc@gmail.com>
2025-10-31 22:09:39 +0530

    Login and Resigter Design

--- project/client/src/App.jsx
+++ project/client/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import {BrowserRouter , Routes , Route} from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     <h1>Movie Tickets App</h1>
+    <BrowserRouter>
+       <Routes>
+         <Route path='/login' element={<Login/>}/>
+         <Route path='/register' element={<Register/>}/>
+       </Routes>
+    </BrowserRouter>
+     
+
+
+  
+    
     </>
   )
 }
