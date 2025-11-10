b2d6419196770b0c198d9f62daee003245ea3562
mrinal1224 <iammrinalkc@gmail.com>
2025-11-10 22:46:49 +0530

    movie api

--- project/server/routes/movie.route.js
+++ project/server/routes/movie.route.js
@@ -23,11 +23,89 @@ movieRouter.post('/add-movie' , async(req , res)=>{
    }
 })
 
+
+
 // update movie
+
+movieRouter.put('/update-movie/:id' , async(req , res)=>{
+    try {
+     const movieId = req.params.id
+     const movie = await Movie.findByIdAndUpdate(movieId , req.body)
+       res.send({
+            success: true,
+            message: 'The movie has been updated!',
+            data: movie
+        })
+    } catch (error) {
+           res.send({
+            success: false,
+            message: 'Server Error'
+        })
+    }
+
+})
+
+
+
+
 // Delete Movie
 
+movieRouter.delete('/delete-movie/:id' , async(req , res)=>{
+    try {
+     const movieId = req.params.id
+     const movie = await Movie.findByIdAndDelete(movieId , req.body)
+       res.send({
+            success: true,
+            message: 'The movie has been updated!',
+            data: movie
+        })
+    } catch (error) {
+           res.send({
+            success: false,
+            message: 'Server Error'
+        })
+    }
+
+})
+
+
 // get all Movies
+movieRouter.get('/all-movies' , async(req , res)=>{
+      try {
+         const allMovies =  await Movie.find()
+         res.send({
+            success: true,
+            message: 'All movies have been fetched!',
+            data: allMovies
+        });
+
+      } catch (error) {
+          res.send({
+            success: false,
+            message: error.message
+        });
+      }
+})
+
 // get a specific Movie
+
+movieRouter.get('/:id' , async(req , res)=>{
+    try {
+        const movie = await Movie.findById(req.params.id)
+         res.send({
+            success: true,
+            message: "Movie fetched successfully!",
+            data: movie
+        })
+    } catch (error) {
+           res.send({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+
 
 
 
