30fc4fed949db0891560d5a973b7a591f2d39686
mrinal1224 <iammrinalkc@gmail.com>
2025-11-12 22:06:40 +0530

    push

--- project/client/src/pages/Admin/index.jsx
+++ project/client/src/pages/Admin/index.jsx
@@ -1,29 +1,24 @@
-import React from 'react'
-import { Tabs } from 'antd'
-import MovieList from './MovieList'
-import TheatreList from './TheatreList'
-
+import React from "react";
+import { Tabs } from "antd";
+import MovieList from "./MovieList";
+import TheatreList from "./TheatreList";
 
 function Admin() {
-   const tabItems = [
-      {
-          key :'1',
-          label:'MovieList',
-          children:<MovieList/>
-      },
+  const tabItems = [
+    {
+      key: "1",
+      label: "MovieList",
+      children: <MovieList />,
+    },
 
-      {
-        key :'2',
-        label:'TheatreList',
-        children:<TheatreList/>
+    {
+      key: "2",
+      label: "TheatreList",
+      children: <TheatreList />,
+    },
+  ];
 
-      }
-    ]
-
-
-  return (
-       <Tabs items={tabItems}/>
-  )
+  return <Tabs items={tabItems} />;
 }
 
-export default Admin
+export default Admin;
