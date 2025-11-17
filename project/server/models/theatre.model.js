ab19df1da370951e054c7db4ff4901eaade56c60
mrinal1224 <iammrinalkc@gmail.com>
2025-11-17 21:29:03 +0530

    model api for theartres

---
+++ project/server/models/theatre.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const theatreSchema = new mongoose.Schema(
+  {
+    name : {
+        type : String,
+        required: true
+    },
+    address:{
+        type:String,
+        required : true
+    },
+    
+    email:{
+
+        type:String,
+        required: true
+    },
+    phone : {
+        type:Number,
+        required: true
+    },
+    isActive:{
+        type:Boolean,
+        default:false
+    },
+
+    // owner:{
+    //     type
+    // },
+
+
+  },
+  { timestamps: true }
+);
+
+const Theatre = mongoose.model("theatre", theatreSchema);
+
+module.exports = Theatre;
