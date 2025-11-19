360986c483ffe79a5803a0926562f96bb6d15476
mrinal1224 <iammrinalkc@gmail.com>
2025-11-19 22:51:22 +0530

    show form done

--- project/client/src/pages/Partner/ShowModal.jsx
+++ project/client/src/pages/Partner/ShowModal.jsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { Modal, Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Table, Form, Row, Col, Input, Select } from "antd";
+import {
+  ArrowLeftOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import { getAllMovies } from "../../calls/movieCalls";
+
+
 
 const ShowModal = ({ isShowModalOpen, setIsShowModalOpen }) => {
+  const [view, setView] = useState("table");
+  const [movies , setMovies] = useState([])
 
-   function handleCancel(){
-    setIsShowModalOpen(false)
-   }
+  function handleCancel() {
+    setIsShowModalOpen(false);
+  }
 
+  const getData = async()=>{
+    try {
+        const movieResponse = await getAllMovies()
+        if(movieResponse.success){
+            console.log(movieResponse.data)
+            setMovies(movieResponse.data)
+        }
+        else{
+            message.error(movieResponse.error);
+        }
+        
+    } catch (error) {
+        message.error(error.message);
+    }
+  }
+
+
+  useEffect(()=>{
+    getData()
+  }, [])
 
   const columns = [
     {
@@ -40,7 +70,192 @@ const ShowModal = ({ isShowModalOpen, setIsShowModalOpen }) => {
   return (
     <>
       <Modal width={1200} onCancel={handleCancel} open={isShowModalOpen}>
-        <Table columns={columns}></Table>
+        <div className="d-flex justify-content-between">
+          <h3>
+            {view === "table"
+              ? "List of Shows"
+              : view === "form"
+              ? "Add Show"
+              : "Edit Show"}
+          </h3>
+          {view === "table" && (
+            <Button type="primary" onClick={() => setView("form")}>
+              Add Show
+            </Button>
+          )}
+        </div>
+
+        {view === "table" && <Table columns={columns} />}
+
+        {view === "form" && (
+          <Form>
+            <Row
+              gutter={{
+                xs: 6,
+                sm: 10,
+                md: 12,
+                lg: 16,
+              }}
+            >
+              <Col span={24}>
+                <Row
+                  gutter={{
+                    xs: 6,
+                    sm: 10,
+                    md: 12,
+                    lg: 16,
+                  }}
+                >
+                  <Col span={8}>
+                    <Form.Item
+                      label="Show Name"
+                      htmlFor="name"
+                      name="name"
+                      className="d-block"
+                      rules={[
+                        { required: true, message: "Show name is required!" },
+                      ]}
+                    >
+                      <Input
+                        id="name"
+                        type="text"
+                        placeholder="Enter the show name"
+                      ></Input>
+                    </Form.Item>
+                  </Col>
+                  <Col span={8}>
+                    <Form.Item
+                      label="Show Date"
+                      htmlFor="date"
+                      name="date"
+                      className="d-block"
+                      rules={[
+                        { required: true, message: "Show date is required!" },
+                      ]}
+                    >
+                      <Input
+                        id="date"
+                        type="date"
+                        placeholder="Enter the show date"
+                      ></Input>
+                    </Form.Item>
+                  </Col>
+                  <Col span={8}>
+                    <Form.Item
+                      label="Show Timing"
+                      htmlFor="time"
+                      name="time"
+                      className="d-block"
+                      rules={[
+                        { required: true, message: "Show time is required!" },
+                      ]}
+                    >
+                      <Input
+                        id="time"
+                        type="time"
+                        placeholder="Enter the show date"
+                      ></Input>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
+                <Row
+                  gutter={{
+                    xs: 6,
+                    sm: 10,
+                    md: 12,
+                    lg: 16,
+                  }}
+                >
+                  <Col span={8}>
+                    <Form.Item
+                      label="Select the Movie"
+                      htmlFor="movie"
+                      name="movie"
+                      className="d-block"
+                      rules={[
+                        { required: true, message: "Movie  is required!" },
+                      ]}
+                    >
+                      <Select
+                        id="movie"
+                        placeholder="Select Movie"
+                        // defaultValue={selectedMovie && selectedMovie.title}
+                        style={{ width: "100%", height: "45px" }}
+                        options={movies.map((movie) => ({
+                          key: movie._id,
+                          value: movie._id,
+                          label: movie.title,
+                        }))}
+                      />
+                    </Form.Item>
+                  </Col>
+                  <Col span={8}>
+                    <Form.Item
+                      label="Ticket Price"
+                      htmlFor="ticketPrice"
+                      name="ticketPrice"
+                      className="d-block"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Ticket price is required!",
+                        },
+                      ]}
+                    >
+                      <Input
+                        id="ticketPrice"
+                        type="number"
+                        placeholder="Enter the ticket price"
+                      ></Input>
+                    </Form.Item>
+                  </Col>
+                  <Col span={8}>
+                    <Form.Item
+                      label="Total Seats"
+                      htmlFor="totalSeats"
+                      name="totalSeats"
+                      className="d-block"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Total seats are required!",
+                        },
+                      ]}
+                    >
+                      <Input
+                        id="totalSeats"
+                        type="number"
+                        placeholder="Enter the number of total seats"
+                      ></Input>
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+            <div className="d-flex gap-10">
+              <Button
+                className=""
+                block
+                onClick={() => {
+                  setView("table");
+                }}
+                htmlType="button"
+              >
+                <ArrowLeftOutlined /> Go Back
+              </Button>
+              <Button
+                block
+                type="primary"
+                htmlType="submit"
+                style={{ fontSize: "1rem", fontWeight: "600" }}
+              >
+                {view === "form" ? "Add the Show" : "Edit the Show"}
+              </Button>
+            </div>
+          </Form>
+        )}
       </Modal>
     </>
   );
