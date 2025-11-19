83f6412674af749ef433f0b396b0eea89f117234
mrinal1224 <iammrinalkc@gmail.com>
2025-11-19 21:51:42 +0530

    push

--- project/server/models/show.model.js
+++ project/server/models/show.model.js
@@ -1,1 +1,52 @@
-// This is one small homework
+const mongoose = require("mongoose");
+
+const showSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    time: {
+      type: String,
+      required: true,
+    },
+
+    totalSeats: {
+      type: Number,
+      required: true,
+    },
+
+    ticketPrice: {
+      type: Number,
+      required: true,
+    },
+    bookedSeats: {
+      type: Array,
+      default: [],
+    },
+
+    movie: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "movies",
+      required: true,
+    },
+
+    theatre: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "theatres",
+      required: true,
+    },
+  },
+
+  { timestamps: true }
+);
+
+const Show = mongoose.model("show", showSchema);
+
+module.exports = Show;
